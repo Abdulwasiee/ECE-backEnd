81cf62faf34c38ded45f8362ac9c1917a90f339f
Abdulwasiee <wasungncom@gmail.com>
2024-10-19 09:43:56 +0300

    updated

--- Controllers/password.controller.js
+++ Controllers/password.controller.js
@@ -1,7 +1,7 @@
 const { changePasswordService, requestPasswordReset, resetNewPassword } = require("../Services/password.service");
 
 const changePassword = async (req, res) => {
-  const { oldPassword, newPassword } = req.body;
+  const { oldPassword, newPassword } = req.body;z
   const userId = req.user.user_id;
 
   if (!oldPassword || !newPassword) {
