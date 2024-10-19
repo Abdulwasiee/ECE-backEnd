d42b49f47bb29d3d9ffda562949c704c39a56ae4
Abdulwasiee <wasungncom@gmail.com>
2024-10-19 10:10:06 +0300

    password router updated

--- Routes/password.router.js
+++ Routes/password.router.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const password = require("../controllers/password.controller");
+const {
+  sendPasswordResetEmail,
+  resetPassword,
+  changePassword,
+} = require("../controllers/password.controller");
 const { authMiddleware } = require("../Middlewire/Auth");
 
 const router = express.Router();
 
 // router to change password
-router.post(
-  "/api/changePassword",
-  authMiddleware([3, 4, 5]),
-  password.changePassword
-);
+router.post("/api/changePassword", authMiddleware([3, 4, 5]), changePassword);
 
-router.post("/api/requestPasswordReset", password.sendPasswordResetEmail);
+router.post("/api/requestPasswordReset", sendPasswordResetEmail);
 
-router.post("/api/resetPassword", password.resetPassword);
+router.post("/api/resetPassword", resetPassword);
 
 module.exports = router;
